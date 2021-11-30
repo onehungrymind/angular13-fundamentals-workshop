0793dea70552931eaf7856f516b7837eddc82e05
Lukas Ruebbelke <lukas.ruebbelke@briebug.com>
2021-11-30 13:33:02 -0700

    update: solution

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,8 +16,13 @@ export class HomeComponent implements OnInit {
     { title: 'Angular Routing' },
     { title: 'Unit Testing Fundamentals' },
   ];
+  selectedLesson: any;
 
   constructor() {}
 
   ngOnInit() {}
+
+  selectLesson(lesson) {
+    this.selectedLesson = lesson;
+  }
 }
