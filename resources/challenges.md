e1b110280f60c327fef9ce0ee03e50afb03103ee
Lukas Ruebbelke <lukas.ruebbelke@briebug.com>
2021-11-30 12:01:21 -0700

    update: live coding example

--- resources/challenges.md
+++ resources/challenges.md
@@ -5,6 +5,11 @@ In the `src/app/courses/courses.component.ts` file, complete the following chall
 - Add event handler to select course
 - Display raw JSON of selected course
 
+In the `src/app/home/home.component.html` file, complete the following challenges.
+- Display lessons using `ngFor`
+- Add event handler to select lesson
+- Display raw JSON of selected lesson
+
 # Module 02: Form Challenges
 In the `src/app/courses/courses.component.ts` file, complete the following challenges.
 - Update the form to show `percentComplete`
--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+  // 1. Render courses in a list
+  // 2. Select a course
+  // 3. Render selected course
+
   courses = [
     {
       id: 1,
@@ -13,12 +17,28 @@ export class CoursesComponent implements OnInit {
       description: 'Learn the fundamentals of Angular 13',
       percentComplete: 26,
       favorite: true
+    },
+    {
+      id: 2,
+      title: 'JavaScript The HARDEST PARTS EVER!',
+      description: 'Learn the JavaScript like a pro! with Will',
+      percentComplete: 26,
+      favorite: true
     }
   ];
+  selectedCourse = null;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectCourse(course) {
+    this.selectedCourse = course;
+  }
+
+  deleteCourse(courseId) {
+    console.log('DELETE COURSE', courseId);
   }
 
 }
